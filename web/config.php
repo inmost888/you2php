e6d968877903801553dd29d506d6d98f4b16698f
inmost888 <72589045+inmost888@users.noreply.github.com>
2020-10-09 20:00:50 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,9 +2,9 @@
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyA511DXu-IMRWtDOrOaBncSucVtRWD6d2Y');
-define('GJ_CODE', $gl);
+define('GJ_CODE', 'TW');
 define('SITE_NAME', 'Hello World!');
 define('TITLENAME', 'Hello World!');
-define('EN2DEKEY', 'AnQUP1XDEF87ewemh58-=--/4:CtzDSh4');
-define('EMAIL', 'None@gmail.com');
+define('EN2DEKEY', 'sdgfewt3t3245ergsg');
+define('EMAIL', 'xiaohemamam@gmail.com');
 ?>
