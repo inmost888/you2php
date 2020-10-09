b6c6a131258ebfa31544716f898230f235d044f5
inmost888 <72589045+inmost888@users.noreply.github.com>
2020-10-09 19:57:10 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyByudd09CeGwJDAUsKrsO_ThY3QkxiDufE');
+define('APIKEY', 'AIzaSyA511DXu-IMRWtDOrOaBncSucVtRWD6d2Y');
 define('GJ_CODE', $gl);
 define('SITE_NAME', 'Hello World!');
 define('TITLENAME', 'Hello World!');
